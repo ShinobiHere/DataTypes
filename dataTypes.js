f02ecfaefb321197cd321251b033361923695931
ShinobiHere <113454114+ShinobiHere@users.noreply.github.com>
2023-10-12 06:00:28 +0000

    dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+// Primitive Types
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn =  true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNumber = 2342736273428373n
+
+// Non Primitive
+const heroes = ["Captain America", "Thor", "SpiderMan"]
+let myName = {
+    name:"Tauha",
+    age: 17,
+}
+const myFunction= function(){
+    console.log("Hello Tauha")
+
+}
+console.log(typeof heroes)
+console.log(typeof myFunction)
